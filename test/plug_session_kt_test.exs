67ad6048dd80e0cb0046161c067b8caac99f4625
Ley Missailidis <moisis.langley@missailidis.ca>
2016-09-11 14:49:22 -0700

    start testing

--- test/plug_session_kt_test.exs
+++ test/plug_session_kt_test.exs
@@ -1,8 +1,24 @@
 defmodule PlugSessionKtTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest PlugSessionKt
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @default_opts [
+    store: :kt
+    key:
+    table: :kt_sessions,
+    max_age: 60
+  ]
+
+  defp put_env do
+    Application.put_env()
   end
+
+  setup_all do
+    Application.stop(:plug_session_kt)
+    put_envs
+    :ok = Application.start(:plug_session_kt)
+    IO.puts "Kyoto Tycoon has started"
+  end
+
+
 end
