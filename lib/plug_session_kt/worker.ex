68bd3931e931e4893236adb7634a1875c35ddb12
Ley Missailidis <moisis.langley@missailidis.ca>
2016-09-12 15:53:24 -0700

    minor fixes toward code readability and docs

--- lib/plug_session_kt/worker.ex
+++ lib/plug_session_kt/worker.ex
@@ -1,5 +1,7 @@
 defmodule PlugSessionKT.Worker do
 
+  @moduledoc false
+
   @spec start_link({module, Keyword.t}) :: {:ok, pid}
   def start_link({module, params}) do
     module.start_link(:undefined, params)
